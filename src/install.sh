c9c32fdba890a16bd253e72007d36e6f6bd7c945
hrayo712 <hrayotorres@gmail.com>
2020-02-28 16:52:13 +0100

    updated the install file for system integration

--- src/install.sh
+++ src/install.sh
@@ -8,7 +8,7 @@ filename="TCG_SDK_2018_02_14"
 
 #Verify if v4l2loopback is installed
 
-if ! dpkg -l > /dev/null; then
+if ! dpkg -l | grep v4l2loopback-dkms > /dev/null; then
 	echo "Installing v4l2loopback-dkms"
 	sudo apt-get install v4l2loopback-dkms
 
@@ -16,7 +16,7 @@ fi
 
 #Verify if libusb-1.0-0-dev is installed
 
-if ! dpkg -l > /dev/null; then
+if ! dpkg -l | grep libusb-1.0-0-dev > /dev/null; then
 	echo "Installing libusb-1.0-0-dev"
 	sudo apt-get install libusb-1.0-0-dev
 fi
@@ -38,11 +38,17 @@ cmake CMakeLists.txt
 
 make
 
+#create a soft symbolic link
+#TODO : When using the symbolic link to run the daemon together with sudo, it doesnt work. For the meantime, we'll use the direct link
+#ln -sf $PWD/lib/libthermalgrabber.so ../../libthermalgrabber.so
+
 popd
 
 pushd ${filename}/TCGrabberUSBV4L2
 
 make
+
+ln -sf $PWD/TCGrabberUSBV4L2d ../../TCGrabberUSBV4L2d
 
 popd
 
